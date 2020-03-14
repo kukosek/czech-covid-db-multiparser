167dc6c6089ef2ac7714d995c090f15f4ed6a29a
Lukas Dulik <kukys410@gmail.com>
2020-03-14 14:38:54 +0100

    Now parses information about each region (kraj)

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import parser
 import subprocess
 import time
-
+import logging
+from datetime import datetime
 checkWaitTime = 20
 pathToConfirmedCsv = "/home/pi/korona/czech-covid-db/records_confirmed.csv"
 pathToRecoveredCsv = "/home/pi/korona/czech-covid-db/records_recovered.csv"
 pathToDeathsCsv = "/home/pi/korona/czech-covid-db/records_deaths.csv"
 pathToCurrentNumbers = "/home/pi/korona/czech-covid-db/current_numbers.json"
-updateCommand = "git_coviddb_update.sh"
+updateCommand = "/home/pi/korona/czech-covid-db-wikipediaparser/git_coviddb_update.sh"
+logFile = "main.log"
+
 
 parsing = parser.Parser()
+logging.basicConfig(filename=logFile, level=logging.DEBUG)
+logging.info("-------------------------------")
+logging.info(datetime.now().strftime("%d.%m. %H:%M")+" Starting check loop")
 
-while True:
-    result = parsing.parse(pathToConfirmedCsv, pathToRecoveredCsv, pathToDeathsCsv, pathToCurrentNumbers)
-    print(result)
-    if result == True:
-        print("Running update command")
-        subprocess.run(updateCommand.split())
-    time.sleep(checkWaitTime)
+try:
+    while True:
+        result = parsing.parse(pathToConfirmedCsv, pathToRecoveredCsv, pathToDeathsCsv, pathToCurrentNumbers)
+        if result == True:
+            logging.info("Running update command")
+            logging.info()
+            
+            logging.info(subprocess.run(updateCommand.split(), capture_output=True).stdout)
+        time.sleep(checkWaitTime)
+except KeyboardInterrupt:
+    logging.info(datetime.now().strftime("%d.%m. %H:%M")+"Interrupted, exiting...")
--- parser.py
+++ parser.py
@@ -7,47 +7,74 @@ import locale
 from datetime import datetime
 import csv
 import json
+import logging
 
 class Parser:
     datetimeFormat = "%Y-%m-%dT%H:%M:%S+01:00"
-    columnNames = ["Record number", "Date", "All"]
+    columnNames = ["Record number", "Date", "All", "hlavní město Praha",
+                   "Středočeský kraj", "Ústecký kraj", "Královéhradecký kraj",
+                   "Zlínský kraj", "Olomoucký kraj", "Pardubický kraj",
+                   "Kraj Vysočina", "Plzeňský kraj", "Jihomoravský kraj",
+                   "Liberecký kraj", "Karlovarský kraj", "Moravskoslezský kraj",
+                   "Jihočeský kraj"]
     def __init__(self):
         locale.setlocale(locale.LC_ALL, 'cs_CZ.utf8')
         pass
 
-    def csvAppendIfNew(self, datetimeObj, value, pathToCsv):
+    def csvAppendIfNew(self, datetimeObj, value, casesPerKraj, pathToCsv):
+        headerRow = []
         rows = []
         newInformation = False
         with open(pathToCsv, mode='r', newline='') as records_file:
+            csvreader = csv.reader(records_file)
+            for row in csvreader:
+                headerRow = row
+                break
+            
             csvreader = csv.DictReader(records_file, fieldnames=self.columnNames)
             i = 0
             for row in csvreader:
-                
-                if i>0:
-                    rowData = []
-                    for columnName in self.columnNames:
-                        rowData.append(row[columnName])
-                    rows.append(rowData)
+                rowData = []
+                for columnName in self.columnNames:
+                    rowData.append(row[columnName])
+                rows.append(rowData)
+        lastRow = rows[-1]
+        if len(rows) == 0 or (len(rows) == 1 and rows[0][0] == self.columnNames[0]) or (len(rows) > 0 and lastRow[2] != value):
+            newInformation = True
+        elif casesPerKraj != None:
+            i = 0
+            for krajName in self.columnNames[3:]:
+                if lastRow[i+3] != casesPerKraj[krajName]:
+                    newInformation = True
+                    break
                 i+=1
-            if len(rows) == 0 or (len(rows) > 0 and rows[-1][2] != value):
-                newInformation = True
+        #check if confirmed cases in kraj is different
+        
+        if len(rows) != 0 and headerRow != self.columnNames:
+            with open(pathToCsv, mode='w', newline='') as records_file:
+                writer = csv.writer(records_file)
+                writer.writerow(self.columnNames)
+                for row in rows:
+                    writer.writerow(row)
         if (newInformation == True):
-            print("New info, saving to", pathToCsv)
+            logging.info("New info, saving to", pathToCsv)
             if datetimeObj.date() == datetime.today().date():
                 datetimeObj = datetime.now()
             elif datetimeObj.date() < datetime.today().date():
                 if (len(rows) > 0):
-                    lastDateWrited = datetime.strptime(rows[-1][1], self.datetimeFormat)
+                    lastDateWrited = datetime.strptime(lastRow[1], self.datetimeFormat)
                     if (lastDateWrited.date() == datetimeObj.date()):
                         #place the date in the somewhere in the half
                         nextDayDatetime = datetimeObj.date() + datetime.timedelta(days=1)
                         datetimeObj = lastDateWrited + ( (nextDayDatetime - lastDateWrited) / 2 )
             else:
-                print("Error: time is greater than my time. Are we time travelling?")
+                logging.error("Error: time is greater than my time. Are we time travelling?")
             datetimeConverted = datetimeObj.strftime(self.datetimeFormat)
-            print(datetimeConverted)
             fileOpenMethod = "a"
             # if no rows are in the file, open it with w+, which will delete the contents. this is to avoid having an empty first line
+            
+            
+            
             if len(rows)==0: fileOpenMethod = "w+"
             with open(pathToCsv,fileOpenMethod, newline='') as records_file:
                 
@@ -56,15 +83,26 @@ class Parser:
                     writer.writerow(self.columnNames)
                 id = 0
                 if len(rows) > 0:
-                    id = rows[-1][0]
+                    id = str(int(rows[-1][0])+1)
                 writer = csv.DictWriter(records_file, fieldnames=self.columnNames)
-                writer.writerow({self.columnNames[0]:id, self.columnNames[1]:datetimeConverted, self.columnNames[2]:value})
+                rowToWrite = {self.columnNames[0]:id, self.columnNames[1]:datetimeConverted, self.columnNames[2]:value}
+                if casesPerKraj != None and  len(casesPerKraj)==len(self.columnNames[3:]):
+                    for key, value in casesPerKraj.items():
+                        rowToWrite[key] = value
+                writer.writerow(rowToWrite)
         return newInformation
 
     def parse(self, pathToConfirmedCSV, pathToRecoveredCSV, pathToDeathsCSV, pathToCurrentNumbersJSON):
-        fp = urllib.request.urlopen("https://cs.wikipedia.org/wiki/Pandemie_COVID-19_v_%C4%8Cesku#Ned%C4%9Ble_1._b%C5%99ezna")
-        html = fp.read().decode("utf8")
-        fp.close
+        try:
+            fp = urllib.request.urlopen("https://cs.wikipedia.org/w/api.php?action=parse&page=Pandemie_COVID-19_v_%C4%8Cesku&prop=text&formatversion=2&format=json")
+            html = json.loads(fp.read().decode("utf8"))["parse"]["text"]
+            fp.close
+        except urllib.HTTPError as e:
+            logging.error('HTTPError = ' + str(e.code))
+        except urllib.URLError as e:
+            logging.error('URLError = ' + str(e.reason))
+        except httplib.HTTPException as e:
+            logging.error('HTTPException')
         
         soup = BeautifulSoup(html, 'html.parser')
 
@@ -72,12 +110,19 @@ class Parser:
               "recovered":{"number":"", "date":""},
               "deaths":     {"number":"", "date":""}}
         update = False
-
-        print()
         # parsing current numbers
+        confirmedPerKraj = {}
         for trTag in soup.find_all('tr'):
             allThTags = trTag.find_all('th')
             if len(allThTags) == 1:
+                if allThTags[0].get_text() == "Rozšíření":
+                    textRozsireni = trTag.find_all("td")[0].get_text()[:-9].replace("," , ";")
+                    infoPerKraj = textRozsireni.split(";")
+                    for krajInfo in infoPerKraj:
+                        krajName, confirmedInKraj = krajInfo.split("(")
+                        krajName = krajName.strip()
+                        confirmedInKraj = confirmedInKraj[:-1].strip()
+                        confirmedPerKraj[krajName] = confirmedInKraj
                 if allThTags[0].get_text() == "Nakažení":
                     textConfirmed = trTag.find_all("td")[0].get_text()
                     num, date = textConfirmed.split("(",2)
@@ -87,9 +132,9 @@ class Parser:
                     all["confirmed"]["number"]=num
                     datetimeConverted = dateobject.strftime(self.datetimeFormat)
                     all["confirmed"]["date"]=datetimeConverted
-
+                    
                     # here comes csv handling
-                    appended = self.csvAppendIfNew(dateobject, num, pathToConfirmedCSV)
+                    appended = self.csvAppendIfNew(dateobject, num, confirmedPerKraj, pathToConfirmedCSV)
                     if appended: update = True
                 if allThTags[0].get_text() == "Zotavení":
                     textConfirmed = trTag.find_all("td")[0].get_text()
@@ -101,7 +146,7 @@ class Parser:
                     datetimeConverted = dateobject.strftime(self.datetimeFormat)
                     all["recovered"]["date"]=datetimeConverted
                     # here comes csv handling
-                    appended = self.csvAppendIfNew(dateobject, num, pathToRecoveredCSV)
+                    appended = self.csvAppendIfNew(dateobject, num, None, pathToRecoveredCSV)
                     if appended: update = True
                 if allThTags[0].get_text() == "Úmrtí":
                     textConfirmed = trTag.find_all("td")[0].get_text()
@@ -113,8 +158,11 @@ class Parser:
                     datetimeConverted = dateobject.strftime(self.datetimeFormat)
                     all["deaths"]["date"]=datetimeConverted
                     # here comes csv handling
-                    appended = self.csvAppendIfNew(dateobject, num, pathToDeathsCSV)
+                    appended = self.csvAppendIfNew(dateobject, num, None, pathToDeathsCSV)
                     if appended: update = True
-        with open(pathToCurrentNumbersJSON, "w+") as jsonfile:
-            json.dump(all, jsonfile)
+                if (update):
+                    pass
+        if(update):
+            with open(pathToCurrentNumbersJSON, "w+") as jsonfile:
+                json.dump(all, jsonfile)
         return update
