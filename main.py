308e176bd3346db225b4780a6aef1bf35ffca2ec
Lukas Dulik <kukys410@gmail.com>
2020-04-01 19:11:31 +0200

    directoies for raspberry

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import time
 import logging
 from datetime import datetime
 checkWaitTime = 20
-pathToTestsUZISCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/uzis/records_tests_uzis.csv"
-pathToConfirmedUZISCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/uzis/records_confirmed_uzis.csv"
-pathToRecoveredUZISCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/uzis/records_recovered_uzis.csv"
-pathToDeathsUZISCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/uzis/records_deaths_uzis.csv"
-pathToImportsUZISCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/uzis/records_imports_uzis.csv"
-pathToAgeGroupsUZISCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/uzis/records_confirmed-agegroups_uzis.csv"
-pathToCurrentNumbersUZIS = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/uzis/current_numbers_uzis.json"
+pathToTestsUZISCsv = "/home/pi/korona/czech-covid-db/uzis/records_tests_uzis.csv"
+pathToConfirmedUZISCsv = "/home/pi/korona/czech-covid-db/uzis/records_confirmed_uzis.csv"
+pathToRecoveredUZISCsv = "/home/pi/korona/czech-covid-db/uzis/records_recovered_uzis.csv"
+pathToDeathsUZISCsv = "/home/pi/korona/czech-covid-db/uzis/records_deaths_uzis.csv"
+pathToImportsUZISCsv = "/home/pi/korona/czech-covid-db/uzis/records_imports_uzis.csv"
+pathToAgeGroupsUZISCsv = "/home/pi/korona/czech-covid-db/uzis/records_confirmed-agegroups_uzis.csv"
+pathToCurrentNumbersUZIS = "/home/pi/korona/czech-covid-db/uzis/current_numbers_uzis.json"
 
-pathToConfirmedCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/records_confirmed.csv"
-pathToRecoveredCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/records_recovered.csv"
-pathToDeathsCsv = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/records_deaths.csv"
-pathToCurrentNumbers = "/media/lukas/Data/Dokumenty/Programovani_Kodovani_Vsehochut/projects-bigger/korona/czech-covid-db/current_numbers.json"
+pathToConfirmedCsv = "/home/pi/korona/czech-covid-db/records_confirmed.csv"
+pathToRecoveredCsv = "/home/pi/korona/czech-covid-db/records_recovered.csv"
+pathToDeathsCsv = "/home/pi/korona/czech-covid-db/records_deaths.csv"
+pathToCurrentNumbers = "/home/pi/korona/czech-covid-db/current_numbers.json"
 
 updateCommand = ""
 logFile = "main.log"
